458f9895e2c5f2ec7b9a52a156b1587459779149
Mr.m <Mr.m>
2020-04-09 16:53:24 +0800

    crontab schedule .

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,8 +27,11 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
+        ### Download resources ###
+        $schedule->command('command:torrent:download')->everyMinute();
+        
+        ### Upload videos ###
+        $schedule->command('do:video:upload')->everyMinute();
     }
 
     /**

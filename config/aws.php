1697b8551f098d0672a11653575196fe6acf751d
Mr.m <Mr.m>
2021-05-29 14:11:55 +0800

    key pair change .

--- config/aws.php
+++ config/aws.php
@@ -3,7 +3,7 @@
 return [
     'cloudfront' => [
         'sign' => [
-            'key_pair_id' => 'K1WZ1VUDT2RXKK',
+            'key_pair_id' => 'K2KBKB531MSF7Q',
             'private_key_file_path' => config_path().DIRECTORY_SEPARATOR.'aws'.DIRECTORY_SEPARATOR.'sign'.DIRECTORY_SEPARATOR.'private_key.pem',
         ],
     ],

4eef9619fbd3a56f5dee5919dc197384385d528c
Mr.m <Mr.m>
2021-06-26 02:25:37 +0800

    bug fixed .

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -19,7 +19,6 @@ class Kernel extends ConsoleKernel
         \App\Console\Commands\ToolsCommand::class,
         \App\Console\Commands\SitemapCommand::class,
         \App\Console\Commands\AwsSignerCommand::class,
-        \App\Console\Commands\AwsFileSyncCommand::class,
         \App\Console\Commands\HlsCommand::class,
     ];
 
